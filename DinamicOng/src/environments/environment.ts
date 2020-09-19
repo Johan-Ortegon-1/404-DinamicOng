f25d7aace3dfc65f5609b8dc68a3bfd226d97ab6
Juan Sebastián Prado Valero <pradosebas@yahoo.com.co>
2020-09-19 10:32:28 -0500

    Hosting Firebase

--- DinamicOng/src/environments/environment.ts
+++ DinamicOng/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig : {
+  apiKey: "AIzaSyBB2zQ18_Fal_pmA3eV4o_HF9qBCbbSBi4",
+  authDomain: "dinamicong2020.firebaseapp.com",
+  databaseURL: "https://dinamicong2020.firebaseio.com",
+  projectId: "dinamicong2020",
+  storageBucket: "dinamicong2020.appspot.com",
+  messagingSenderId: "964989875789",
+  appId: "1:964989875789:web:a2cecd0e721305ff358e04",
+  measurementId: "G-FL8KPGXK32"
+  }
 };
 
 /*
