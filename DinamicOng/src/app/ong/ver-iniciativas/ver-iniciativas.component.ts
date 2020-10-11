a841ebdfcdfa8ae71972569a0d3fd035065ab07d
Johan Daniel Ortegon Parra <johandanielortegonparra@gmail.com>
2020-10-10 21:25:27 -0500

    -garbage

--- DinamicOng/src/app/ong/ver-iniciativas/ver-iniciativas.component.ts
+++ DinamicOng/src/app/ong/ver-iniciativas/ver-iniciativas.component.ts
@@ -1,40 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Iniciativa } from '../../models/iniciativa';
+import { OngService } from '../services/ong.service';
 
 @Component({
   selector: 'app-ver-iniciativas',
   templateUrl: './ver-iniciativas.component.html',
-  styleUrls: ['./ver-iniciativas.component.css']
+  styleUrls: ['./ver-iniciativas.component.css'],
 })
 export class VerIniciativasComponent implements OnInit {
+  constructor(private ongServ: OngService) {}
+  public iniciativas: Array<Iniciativa> = [];
+  public creadas = false;
 
-  constructor() { }
-  public iniciativas : Array<Iniciativa> = [];
-  public creadas= false;
-  
-  ini : Iniciativa = new Iniciativa ();
-  ini2 : Iniciativa = new Iniciativa ();
-  ini3 : Iniciativa = new Iniciativa ();
-  
-  
+  ini: Iniciativa = new Iniciativa();
+  ini2: Iniciativa = new Iniciativa();
+  ini3: Iniciativa = new Iniciativa();
 
   ngOnInit(): void {
-     this.ini.nombre="Mancos de america";
-     this.ini2.nombre="Barriendo el desierto";
-     this.ini3.nombre="La ezquina de la casa redonda";
+    this.ini.nombre = 'Mancos de america';
+    this.ini2.nombre = 'Barriendo el desierto';
+    this.ini3.nombre = 'La ezquina de la casa redonda';
 
-     this.ini.imagenes=['imagen1','imagen2'];
-     this.ini2.imagenes=['imagen3','imagen4'];
-     this.ini3.imagenes=['imagen5','imagen6'];
+    this.ini.imagenes = ['imagen1', 'imagen2'];
+    this.ini2.imagenes = ['imagen3', 'imagen4'];
+    this.ini3.imagenes = ['imagen5', 'imagen6'];
 
-     this.iniciativas.push(this.ini);
-     this.iniciativas.push(this.ini2);
-     this.iniciativas.push(this.ini3);
-
+    this.iniciativas.push(this.ini);
+    this.iniciativas.push(this.ini2);
+    this.iniciativas.push(this.ini3);
   }
   // back() {
   //   this.router.navigate(['/conductor/inicio-conductor']);
   // }
-  
-
 }
