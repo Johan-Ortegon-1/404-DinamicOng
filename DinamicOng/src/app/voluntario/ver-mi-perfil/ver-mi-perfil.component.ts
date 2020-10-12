53378de5fee77ebc77755abc847c43f133eb95b5
Lorduror <camilomoreno@javeriana.edu.co>
2020-10-11 20:36:35 -0500

    voluntario acceso datos

--- DinamicOng/src/app/voluntario/ver-mi-perfil/ver-mi-perfil.component.ts
+++ DinamicOng/src/app/voluntario/ver-mi-perfil/ver-mi-perfil.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Voluntario } from '../../models/voluntario';
+import { VoluntarioService } from '../services/voluntario.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Conocimiento } from '../../models/conocimiento';
 
 @Component({
   selector: 'app-ver-mi-perfil',
@@ -7,9 +14,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerMiPerfilComponent implements OnInit {
 
-  constructor() { }
+  public voluntario: Voluntario;
+  public uid: string;
+  public conocimientos: Conocimiento[] = [];
+  public conocimiento: Conocimiento;
+  public prueb: string;
+  public idim: string[];
+  public telefonos: string[];
+  constructor(private voluntarioServices: VoluntarioService, private configC: NgbCarouselConfig) {
+    configC.interval = 5000;
+    configC.pauseOnHover = true;
+   }
 
   ngOnInit(): void {
-  }
+    this.voluntario = new Voluntario();
+    this.uid = 'fEZlOu94IEeV27fKfRBGINrj9zK2';
+
+    this.voluntarioServices.consultarVoluntarioByID(this.uid).then(resp  => {
+      this.voluntario = resp.data() as Voluntario;
+      this.prueb = this.voluntario.nombre;
+      this.conocimientos = this.voluntario.habilidades;
+      this.idim = this.voluntario.idiomas;
+      this.telefonos = this.voluntario.telefonos;
+
+  });
+}
 
 }
